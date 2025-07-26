57c3f88165371df8a7208276e090f1d6e16b3988
unknown <ce.rumiakter@gmail.com>
2025-07-26 18:33:47 +0600

    update

--- src/pages/Home/Banner2/Banner2.jsx
+++ src/pages/Home/Banner2/Banner2.jsx
@@ -20,21 +20,21 @@ const Banner2 = () => {
                 <SwiperSlide className='relative'>
                     <div className='absolute inset-y-0 md:left-56 left-6 top-30 md:top-50 text-left'>
                         <h1 className='text-2xl md:text-5xl font-extrabold mb-2 md:mb-8 dark:text-black'>BLACK FRIDAY <span className='text-[#D42C7C]'>SALE</span></h1>
-                        <button className='btn border-[#D42C7C] text-[#D42C7C] md:text-lg hover:bg-[#D42C7C] dark:bg-white hover:text-white'>Shop Now</button>
+                        <Link to="/shop"><button className='btn border-[#D42C7C] text-[#D42C7C] md:text-lg hover:bg-[#D42C7C] dark:bg-white hover:text-white'>Shop Now</button></Link>
                     </div>
                     <img src="https://i.ibb.co/C34W09vC/Frame-12-1.png" alt="" />
                 </SwiperSlide>
                 <SwiperSlide className='relative'>
                     <div className='absolute inset-y-0 md:left-56 left-6 top-30 md:top-50 text-left'>
                         <h1 className='text-2xl md:text-5xl font-extrabold mb-2 md:mb-8 dark:text-black'>BLACK FRIDAY <span className='text-[#D42C7C]'>SALE</span></h1>
-                        <button className='btn border-[#D42C7C] text-[#D42C7C] md:text-lg hover:bg-[#D42C7C] dark:bg-white hover:text-white'>Shop Now</button>
+                        <Link to="/shop"><button className='btn border-[#D42C7C] text-[#D42C7C] md:text-lg hover:bg-[#D42C7C] dark:bg-white hover:text-white'>Shop Now</button></Link>
                     </div>
                     <img src="https://i.ibb.co/qM9NBbSC/Rectangle-1.png" alt="" />
                 </SwiperSlide>
                 <SwiperSlide className='relative'>
                     <div className='absolute inset-y-0 md:left-56 left-6 top-30 md:top-50 text-left'>
                         <h1 className='text-2xl md:text-5xl font-extrabold mb-2 md:mb-8 dark:text-black'>BLACK FRIDAY <span className='text-[#D42C7C]'>SALE</span></h1>
-                        <button className='btn border-[#D42C7C] text-[#D42C7C] md:text-lg hover:bg-[#D42C7C] dark:bg-white hover:text-white'>Shop Now</button>
+                        <Link to="/shop"><button className='btn border-[#D42C7C] text-[#D42C7C] md:text-lg hover:bg-[#D42C7C] dark:bg-white hover:text-white'>Shop Now</button></Link>
                     </div>
                     <img src="https://i.ibb.co/TxKFB4D3/Frame-12.png" alt="" />
                 </SwiperSlide>
