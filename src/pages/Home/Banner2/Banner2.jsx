a66a6742b5a4e47c75a88b85a12981a40aa030ba
unknown <ce.rumiakter@gmail.com>
2025-07-27 18:12:02 +0600

    update

--- src/pages/Home/Banner2/Banner2.jsx
+++ src/pages/Home/Banner2/Banner2.jsx
@@ -15,28 +15,28 @@ const Banner2 = () => {
                         <h1 className='text-2xl md:text-5xl font-extrabold mb-2 md:mb-8 dark:text-black'>BLACK FRIDAY <span className='text-[#D42C7C]'>SALE</span></h1>
                         <Link to="/shop"><button className='btn border-[#D42C7C] text-[#D42C7C] md:text-lg hover:bg-[#D42C7C] dark:bg-white hover:text-white'>Shop Now</button></Link>
                     </div>
-                    <img src="https://i.ibb.co/Cs99HrX2/Frame-12-2.png" alt="" />
+                    <img src="https://i.ibb.co/Cs99HrX2/Frame-12-2.png" alt="slider1" />
                 </SwiperSlide>
                 <SwiperSlide className='relative'>
                     <div className='absolute inset-y-0 md:left-56 left-6 top-30 md:top-50 text-left'>
                         <h1 className='text-2xl md:text-5xl font-extrabold mb-2 md:mb-8 dark:text-black'>BLACK FRIDAY <span className='text-[#D42C7C]'>SALE</span></h1>
                         <Link to="/shop"><button className='btn border-[#D42C7C] text-[#D42C7C] md:text-lg hover:bg-[#D42C7C] dark:bg-white hover:text-white'>Shop Now</button></Link>
                     </div>
-                    <img src="https://i.ibb.co/C34W09vC/Frame-12-1.png" alt="" />
+                    <img src="https://i.ibb.co/C34W09vC/Frame-12-1.png" alt="slider2" />
                 </SwiperSlide>
                 <SwiperSlide className='relative'>
                     <div className='absolute inset-y-0 md:left-56 left-6 top-30 md:top-50 text-left'>
                         <h1 className='text-2xl md:text-5xl font-extrabold mb-2 md:mb-8 dark:text-black'>BLACK FRIDAY <span className='text-[#D42C7C]'>SALE</span></h1>
                         <Link to="/shop"><button className='btn border-[#D42C7C] text-[#D42C7C] md:text-lg hover:bg-[#D42C7C] dark:bg-white hover:text-white'>Shop Now</button></Link>
                     </div>
-                    <img src="https://i.ibb.co/qM9NBbSC/Rectangle-1.png" alt="" />
+                    <img src="https://i.ibb.co/qM9NBbSC/Rectangle-1.png" alt="slider3" />
                 </SwiperSlide>
                 <SwiperSlide className='relative'>
                     <div className='absolute inset-y-0 md:left-56 left-6 top-30 md:top-50 text-left'>
                         <h1 className='text-2xl md:text-5xl font-extrabold mb-2 md:mb-8 dark:text-black'>BLACK FRIDAY <span className='text-[#D42C7C]'>SALE</span></h1>
                         <Link to="/shop"><button className='btn border-[#D42C7C] text-[#D42C7C] md:text-lg hover:bg-[#D42C7C] dark:bg-white hover:text-white'>Shop Now</button></Link>
                     </div>
-                    <img src="https://i.ibb.co/TxKFB4D3/Frame-12.png" alt="" />
+                    <img src="https://i.ibb.co/TxKFB4D3/Frame-12.png" alt="slider4" />
                 </SwiperSlide>
 
             </Swiper>
--- src/pages/Home/OfferBanner/OfferBanner.jsx
+++ src/pages/Home/OfferBanner/OfferBanner.jsx
@@ -8,10 +8,10 @@ const OfferBanner = () => {
                 <p>See all</p>
             </div>
             <Marquee>
-                <img src="https://i.ibb.co/21cGrphf/1-3.png" alt="" />
-                <img className="ml-8 mr-8" src="https://i.ibb.co/XrcjTgnj/2-2.png" alt="" />
+                <img src="https://i.ibb.co/21cGrphf/1-3.png" alt="offerbanner1" />
+                <img className="ml-8 mr-8" src="https://i.ibb.co/XrcjTgnj/2-2.png" alt="offerbanner2" />
                 <img src="https://i.ibb.co/QjnLc3Bb/3-2.png" alt="" />
-                <img className="ml-8 mr-8" src="https://i.ibb.co/ZppXfWsp/b-4.png" alt="" />
+                <img className="ml-8 mr-8" src="https://i.ibb.co/ZppXfWsp/b-4.png" alt="offerbanner3" />
             </Marquee>
         </div>
     );
